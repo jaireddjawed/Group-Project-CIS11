7a0cb9adb507679467ed0200b7d23e51730f9208
Jaired Jawed <jaireddjawed.7@gmail.com>
2018-05-29 14:55:08 -0700

    Update Print5.asm

--- Print5.asm
+++ Print5.asm
@@ -5,12 +5,11 @@ TITLE Print5
 option casemap :none                    ; case sensitive
 
 ; include necessary libraries
-include \masm32\include\masm32rt.inc
-
-include \masm32\include\windows.inc
+include \masm32\include\windows.inc ; always first
 include \masm32\macros\macros.asm
 
 include \masm32\include\masm32.inc
+include \masm32\include\masm32rt.inc
 include \masm32\include\gdi32.inc
 include \masm32\include\user32.inc
 include \masm32\include\kernel32.inc
